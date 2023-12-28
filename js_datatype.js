69f6a2a288a630d95bde9d0e705e7278a44850e3
parmar-viral <parmarviral8324@gmail.com>
2023-12-29 05:27:53 +0530

    js datatype

---
+++ js_datatype.js
@@ -0,0 +1,17 @@
+/*
+string
+number
+boolean
+object
+undefined
+*/
+let str="javascript"; //string datatype
+let id=23345; //number datatype
+let ans=true; //boolean datatype
+let val=null; //null datatype=>standalone value means empty value
+let num; //undefined datatype=>value not assign to the variable
+console.log("datatype="+typeof str);
+console.log("datatype="+typeof id);
+console.log("datatype="+typeof ans);
+console.log("datatype="+typeof val);
+console.log("datatype="+typeof num);
